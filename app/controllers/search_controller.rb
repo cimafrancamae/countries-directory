2c6d6464007669c0bf6de4d90d24c0690ce749e5
maeci <cimafrancamae@gmail.com>
2024-05-09 06:35:21 +0800

    fix: search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,19 +3,19 @@ require 'uri'
 class SearchController < CountriesController
   def search_region
     query = params[:query]
-    region = URI.encode_www_form_component(query)
-    @countries = @client.region(region)
+    if query.downcase == "all"
+      @countries = @client.all
+    else
+      region = URI.encode_www_form_component(query)
+      @countries = @client.region(region)
+    end
     render turbo_stream:
       turbo_stream.update("countries", partial: "countries", locals: { countries: @countries})
   end
 
   def search_country
     query = params[:query]
-    # name = URI.encode_www_form_component(query)
-    @countries = @client.all
-    @countries.select do |country|
-      country["name"].downcase.include?(query) || country["name"].downcase.include?(query)
-    end
+    @countries = @client.name(query)
     render turbo_stream:
       turbo_stream.update("countries", partial: "countries", locals: { countries: @countries})
   end
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,20 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class SearchController extends Controller {
-  static targets = ["input", "country"];
+  static targets = ["input", "category"];
 
   connect() {}
 
   search(event) {
     event.preventDefault();
-    const query = this.inputTarget.value.toLowerCase();
-    this.countryTargets.forEach((country) => {
-      const name = country.dataset.name.toLowerCase();
-      if (name.includes(query)) {
-        country.classList.remove("hidden");
-      } else {
-        country.classList.add("hidden");
-      }
-    });
+    this.inputTarget.innerText = this.categoryTarget.innerText
+    document.getElementById("dropdown").classList.add("hidden")
   }
 }
